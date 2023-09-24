786fb54200bde0fbc306a1f367daccac9b16def8
justicecoder <109813073+justice-nana-ellis@users.noreply.github.com>
2023-09-24 01:07:39 +0000

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { validateToken } from '../middlewares/tokenValidater'
-import {signup, signin, resetPassword, forgetPassword, resetPasswordWithLink} from '../controllers/authController';
+import {signup, signin, resetPassword, forgetPassword, resetPasswordWithLink, logout} from '../controllers/authController';
 
 const router = express.Router();
 
 router.post('/signup', signup)
       .post('/signin', signin)
       .post('/reset-password', validateToken, resetPassword)
-      .post('/forget-password', forgetPassword);
+      .post('/forget-password', forgetPassword)
+      .post('/logout', logout);
 router.get('/reset-password', resetPasswordWithLink);
 
 export default router;
